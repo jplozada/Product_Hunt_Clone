cfc7425e5a3e9e0cbac5c0a4ecc0801a7f23a56e
jplozada <juxnpx@gmail.com>
2020-12-13 18:18:57 -0400

    Navigation buttons CSS

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,56 +1,79 @@
-import React from 'react';
-import Buscar from '../ui/Buscar';
-import Navegacion from './Navegacion';
-import Link from 'next/link';
-import styled from '@emotion/styled';
-import {css} from '@emotion/react';
+import React from "react";
+import Link from "next/link";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import Buscar from "../ui/Buscar";
+import Navegacion from "./Navegacion";
+import Boton from "../ui/Boton";
 
 const ContenedorHeader = styled.div`
-    max-width: 1200px;
-    width: 95%;
-    margin: 0 auto;
-    @media (min-width: 768px) {
-        display: flex;
-        justify-content: space-between;
-    }
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
 `;
 
 const Logo = styled.p`
-    color: var(--naranja);
-    font-size: 4rem;
-    line-height: 0;
-    font-weight: 700;
-    font-family: 'Roboto Slab', serif;
-    margin-right: 2rem;
+  color: var(--naranja);
+  font-size: 4rem;
+  line-height: 0;
+  font-weight: 700;
+  font-family: "Roboto Slab", serif;
+  margin-right: 2rem;
 `;
 
 const Header = () => {
-    return (
-        <header
-            css={css`
-                border-bottom: 2px solid var(--gris3);
-                padding: 1rem 0;
-            `}
+  const usuario = false;
+
+  return (
+    <header
+      css={css`
+        border-bottom: 2px solid var(--gris3);
+        padding: 1rem 0;
+      `}
+    >
+      <ContenedorHeader>
+        <div>
+          <Link href="/">
+            <Logo>P</Logo>
+          </Link>
+          <Buscar />
+          <Navegacion />
+        </div>
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+          `}
         >
-            <ContenedorHeader>
-                <div>
-                    <Link href="/">
-                        <Logo>P</Logo>
-                    </Link>
-                    {/* Buscador aqui */}
-                    <Buscar/>
-                    {/* Nav aqui */}
-                    <Navegacion/>
-                </div>
-                <div>
-                    <p>Hola: Juan</p>
-                    <button type="button">Cerrar Sesión</button>
-                    <Link href="/">Login</Link>
-                    <Link href="/">Crear Cuenta</Link>
-                </div>
-            </ContenedorHeader>
-        </header>
-    );
-}
- 
+          {usuario ? (
+            <>
+              <p
+                css={css`
+                  margin-right: 2rem;
+                `}
+              >
+                Hola: Juan
+              </p>
+              <Boton bgColor="true">Cerrar Sesión</Boton>
+            </>
+          ) : (
+            <>
+              <Link href="/">
+                <Boton bgColor="true">Login</Boton>
+              </Link>
+              <Link href="/">
+                <Boton>Crear Cuenta</Boton>
+              </Link>
+            </>
+          )}
+        </div>
+      </ContenedorHeader>
+    </header>
+  );
+};
+
 export default Header;
